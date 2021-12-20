f5db0e70f8835e733fbe21ddbb80264217261fd1
trt14 <tarooti14dev@gmail.com>
2021-12-20 10:26:42 +0300

    Initial Commit

---
+++ BuketListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuketListApp
+//
+//  Created by Tarooti on 16/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
